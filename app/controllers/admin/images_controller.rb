80bd9444ec9d5c6613b6c69e390d8a57a787fd9c
DBurns <dburns@DBurnsMac.local>
2010-01-31 18:25:11 -0200

    admin

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -2,7 +2,6 @@ class Admin::ImagesController < Admin::AdminController
   belongs_to :image_gallery
   
   cache_sweeper :image_gallery_sweeper
-  edit.before{expire_page "/photos/#{parent_object.permalink}"}
   create.wants.html {redirect_to admin_image_gallery_images_path(parent_object)}
   update.wants.html {redirect_to admin_image_gallery_images_path(parent_object)}
   
